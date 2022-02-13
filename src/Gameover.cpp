5aad0e0171ed5e709a905046c08d9d60c269d42e
PaulinLec <paulin.leclercq@epitech.eu>
2022-02-13 14:29:25 +0100

    [+] adding game over txt and make tg

--- src/Gameover.cpp
+++ src/Gameover.cpp
@@ -25,6 +25,7 @@ static const char *quotes[9] =
 void draw_game_over(Window &win, int reset)
 {
     static int num = rand() % 9;
+    sf::Text over;
     sf::Text mot_quote;
     sf::Vector2u pos = win.getSize();
 
@@ -35,13 +36,32 @@ void draw_game_over(Window &win, int reset)
     pos.x /= 2;
     pos.y /= 2;
 
+    init_text(over, "Game over");
+    load_text(over, "Game over");
+
     init_text(mot_quote, quotes[num]);
     load_text(mot_quote, quotes[num]);
 
     sf::FloatRect size = mot_quote.getGlobalBounds();
     pos.x -= size.width / 2;
     pos.y -= size.height / 2;
+    pos.y += 30;
 
     mot_quote.setPosition(pos.x, pos.y);
+
+    pos = win.getSize();
+    over.setCharacterSize(50);
+    size = over.getGlobalBounds();
+
+    pos.x /= 2;
+    pos.y /= 2;
+    pos.x -= size.width / 2;
+    pos.y -= size.height / 2;
+    pos.x -= 30;
+    pos.y -= 100;
+
+    over.setPosition(pos.x, pos.y);
+
     win.draw(mot_quote);
+    win.draw(over);
 }
